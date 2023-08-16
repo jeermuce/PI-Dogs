86ca53d0780545a870bef6c6b655cf20ef82f045
nesmun <jesus.emc@hotmail.com>
2023-08-15 22:58:15 -0600

    - [x] GET /dogs, - [x] POST /dogs, - [x] GET /temperaments

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,60 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define(
+        "dog",
+        {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            num: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            image: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                isUrl: true,
+                defaultValue: "https://imgur.com/a/fCKhvC3",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                UNIQUE: true,
+            },
+            height: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: "unknown",
+            },
+            height_imperial: {
+                type: DataTypes.STRING,
+                defaultValue: "unknown",
+                allowNull: false,
+            },
+            weight: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: "unknown",
+            },
+            weight_imperial: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: "unknown",
+            },
+            life_span: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: "unknown",
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
 };
---
+++ api/src/models/Temperament.js
@@ -0,0 +1,21 @@
+const { DataTypes, Sequelize } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "temperament",
+        {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
+};
