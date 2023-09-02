3f0fffcaccfd69d5eacafaccef00cf87dc8e943f
nesmun <jesus.emc@hotmail.com>
2023-09-02 15:30:42 -0600

    fixed cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db.js");
 
@@ -14,7 +15,8 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
+server.use(cors());
+/* server.use((req, res, next) => {
     const allowedOrigins = ["*"]; // TODO: replace with deployed client URL
 
     const origin = req.headers.origin;
@@ -33,7 +35,7 @@ server.use((req, res, next) => {
     );
 
     next();
-});
+}); */
 
 server.use("/", routes);
 
