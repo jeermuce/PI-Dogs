10dcc8930114dacbee3f6642056fd75092a30602
jeermuce <ernesto_mc.dev@proton.me>
2024-08-26 18:43:35 -0600

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
 import "./App.css";
 
@@ -9,15 +10,17 @@ import Create from "./views/create/Create";
 import Landing from "./views/landing/Landing";
 import Details from "./views/details/Details";
 function App() {
-    return (
-        <section className="App">
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/create" element={<Create />} />
-                <Route path="/home/:id" element={<Details />} />
-            </Routes>
-        </section>
-    );
+	console.log(import.meta.env.VITE_SERVER_URL);
+
+	return (
+		<section className="App">
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/create" element={<Create />} />
+				<Route path="/home/:id" element={<Details />} />
+			</Routes>
+		</section>
+	);
 }
 export default App;
